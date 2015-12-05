9c7b2442b7e62b4d495d4fc8af16e8ca8fb7c05a
JryonGitHub <18801981229@163.com>
2015-12-05 20:15:34 +0800

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -1,5 +1,11 @@
 source 'https://rubygems.org'
 
+gem 'will_paginate'
+
+gem 'bootstrap-will_paginate'
+
+gem 'faker', '1.4.2'
+
 gem 'bcrypt', '3.1.7'
 
 gem 'bootstrap-sass', '~> 3.2.0.2'
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,44 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+
+	def setup
+		@user = users(:jry)
+		@other_user = users(:zhaodan)
+	end
+	
   test "should get new" do
     get :new
     assert_response :success
   end
+	
+	test "should redirect edit when not logged in" do
+		get :edit, id: @other_user
+		assert_redirected_to login_url
+	end
+	test "should redirect update when not logged in" do
+		patch :update, id: @other_user, user: { name: @user.name, email: @user.email }
+		assert_redirected_to login_url
+	end
 
+	test "should redirect index when not logged in" do
+		get :index
+		assert_redirected_to login_url
+	end
+
+	test "should redirect destroy when not logged in" do
+		assert_no_difference 'User.count' do
+			delete :destroy,  id: @user
+		end
+		assert_redirected_to login_url
+	end
+	
+	test "should redirect destroy when logged in as a non-admin" do
+		log_in_as(@other_user)
+		assert_no_difference 'User.count' do
+			delete :destroy, id: @user
+		end
+		assert_redirected_to root_url
+	end
+	
 end
