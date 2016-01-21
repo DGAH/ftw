f2eaa617b2a711a051a6fc4da7dc8444a0ef6c5c
独孤安河 <1346952149@qq.com>
2016-01-21 08:51:38 +0000

    prepare for create recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,9 +3,22 @@ class RecipesController < ApplicationController
     before_action :logged_in_user, only: [:create, :destroy]
     
     def create
+        @recipe = current_user.recipes.build(recipe_params) 
+        if @recipe.save
+            flash[:success] = "You have created a recipe!" 
+            redirect_to root_url 
+        else 
+            #render 'static_pages/home' 
+            render 'users/show'
+        end 
     end
     
     def destroy
     end
     
+private
+    def recipe_params 
+        params.require(:recipe).permit(:name) 
+    end
+
 end
