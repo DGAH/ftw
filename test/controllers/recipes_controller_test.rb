56301326c1ff2add923033f255caa25940407499
独孤安河 <1346952149@qq.com>
2016-01-21 12:57:45 +0000

    add test 'should redirect destory for wrong recipe'

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -4,4 +4,12 @@ class RecipesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should redirect destroy for wrong recipe" do
+    log_in_as(users(:jry)) 
+    recipe = recipes(:gtv) 
+    assert_no_difference 'Recipe.count' do 
+      delete :destroy, id: recipe
+    end 
+    assert_redirected_to root_url 
+  end
 end
